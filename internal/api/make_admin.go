521c974f617c65bc479f036efba6ab48975ce13e
sharma.yash <sharma.yash@razorpay.com>
2023-07-18 23:24:50 +0530

    now backend api nake a list of people admin together

--- internal/api/make_admin.go
+++ internal/api/make_admin.go
@@ -3,13 +3,14 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	"github.com/hashicorp-forge/hermes/pkg/models"
 	"github.com/hashicorp/go-hclog"
 	"gorm.io/gorm"
-	"net/http"
 )
 
-// MakeUserAdminHandler handles the API request to make a user an admin.
+// MakeUserAdminHandler handles the API request to make users admin.
 func MakeUserAdminHandler(log hclog.Logger, db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
@@ -19,9 +20,9 @@ func MakeUserAdminHandler(log hclog.Logger, db *gorm.DB) http.Handler {
 
 		switch r.Method {
 		case "POST":
-			// Decode the request to get the email ID of the user to be made an admin.
+			// Decode the request to get the list of email IDs of the users to be made admins.
 			var req struct {
-				Email string `json:"email"`
+				Emails []string `json:"emails"`
 			}
 			if err := decodeRequest(r, &req); err != nil {
 				log.Error("error decoding make user admin request", "error", err)
@@ -29,26 +30,27 @@ func MakeUserAdminHandler(log hclog.Logger, db *gorm.DB) http.Handler {
 				return
 			}
 
-			var user models.User
-			if err := db.Where("email_address = ?", req.Email).First(&user).Error; err != nil {
-				log.Error("error finding user", "error", err)
-				http.Error(w, "User not found", http.StatusNotFound)
-				return
-			}
+			// Find and update each user in the list.
+			for _, email := range req.Emails {
+				var user models.User
+				if err := db.Where("email_address = ?", email).First(&user).Error; err != nil {
+					log.Error("error finding user", "error", err)
+					continue // Skip to the next user if the current one is not found.
+				}
 
-			// Update the user's role to "admin" (assuming "Role" is the field in the User model that stores roles).
-			user.Role = models.Admin
-			if err := db.Save(&user).Error; err != nil {
-				log.Error("error updating user role", "error", err)
-				http.Error(w, "Error updating user role", http.StatusInternalServerError)
-				return
+				// Update the user's role to "admin" (assuming "Role" is the field in the User model that stores roles).
+				user.Role = models.Admin
+				if err := db.Save(&user).Error; err != nil {
+					log.Error("error updating user role", "error", err)
+					continue // Skip to the next user if there is an error updating the role.
+				}
 			}
 
 			// Send success response with success message.
 			response := struct {
 				Message string `json:"message"`
 			}{
-				Message: "User is now an admin",
+				Message: "Users are now admins",
 			}
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
